e7bae1f76bb7316163104560b69a9294094f90b2
Earl Adams <earladams2000@gmail.com>
2022-07-08 18:08:29 -0400

    tested api in console

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,12 @@ let weather = {
     "apiKey": "a649ac03335b61b88b42edeea070ca64",
     fetchWeather: function () {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=a649ac03335b61b88b42edeea070ca64"
-            ).then((response) => response.json())
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + appKey
+            )
+            .then((response) => response.json())
             .then((data) => console.log(data));
     },
 } 
