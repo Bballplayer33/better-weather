034a1e5420415758865660c16dc93f750c6abb2e
Earl Adams <earladams2000@gmail.com>
2022-07-11 11:23:14 -0400

    fixed spelling errors

--- assets/script.js
+++ assets/script.js
@@ -12,14 +12,14 @@ let weather = {
     },
     displayWeather: function(data) {
         const {name} = data;
-        const {icon , describtion} = data.weather[0];
+        const {icon , description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name,icon,describtion,temp,humidity,speed);
+        console.log(name,icon,description,temp,humidity,speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".describtion").innerText = describtion;
+        document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "mp/h";
