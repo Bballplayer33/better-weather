c58e2d5467da72d6c0195592392568fc3fceb731
Earl Adams <earladams2000@gmail.com>
2022-07-11 11:03:14 -0400

    bug fix for api parameter

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let weather = {
     "apiKey": "a649ac03335b61b88b42edeea070ca64",
-    fetchWeather: function () {
+    fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
@@ -16,20 +16,20 @@ let weather = {
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
         console.log(name,icon,describtion,temp,humidity,speed);
-        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".describtion").innerHTML = describtion;
-        document.querySelector(".temp").innerHTML = temp + "°F";
-        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "mp/h";
+        document.querySelector(".describtion").innerText = describtion;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "mp/h";
 
     },
     search: function () {
-        document.querySelector(".search-bar").value
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
 document.querySelector(".search button").addEventListener("click" , function () {
-
-} )
+     weather.search();
+});
